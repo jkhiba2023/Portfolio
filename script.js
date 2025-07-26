31ef2a51d94d7745c7a80893a64567ece87113fa
jkhiba2023 <khan.jabir1055@gmail.com>
2025-07-27 05:26:46 +0530

    Adding animation to the designation

---
+++ script.js
@@ -0,0 +1,8 @@
+/*=====================================================Typing Animation============================================*/
+var typed = new Typed(".typing", {
+  strings: ["Front-End Engineer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+});
+
